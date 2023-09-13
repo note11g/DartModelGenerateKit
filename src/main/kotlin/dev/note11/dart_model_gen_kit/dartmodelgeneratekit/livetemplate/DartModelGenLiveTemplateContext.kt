049037d5a4cc98afc0ffad23262a80cd95207c2b
김승빈 <note11@kakao.com>
2023-09-14 08:05:57 +0900

    Fix: Change Deprecated API and JVM&Kotlin Version

--- src/main/kotlin/dev/note11/dart_model_gen_kit/dartmodelgeneratekit/livetemplate/DartModelGenLiveTemplateContext.kt
+++ src/main/kotlin/dev/note11/dart_model_gen_kit/dartmodelgeneratekit/livetemplate/DartModelGenLiveTemplateContext.kt
@@ -3,7 +3,7 @@ package dev.note11.dart_model_gen_kit.dartmodelgeneratekit.livetemplate
 import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
 
-class DartModelGenLiveTemplateContext: TemplateContextType("DartModelGen", "DMG") {
+class DartModelGenLiveTemplateContext: TemplateContextType("DartModelGen") {
 
     override fun isInContext(templateActionContext: TemplateActionContext): Boolean {
         return templateActionContext.file.name.endsWith(".dart")
