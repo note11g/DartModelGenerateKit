bd02e51d9f7f64bc501d1eeb5ce2537ad321f898
김승빈 <note11@kakao.com>
2023-09-15 22:35:40 +0900

    Fix: Fix bug(parse issue when comment written)

--- src/main/kotlin/dev/note11/dart_model_gen_kit/dartmodelgeneratekit/util/DartLangParseUtil.kt
+++ src/main/kotlin/dev/note11/dart_model_gen_kit/dartmodelgeneratekit/util/DartLangParseUtil.kt
@@ -5,6 +5,7 @@ import dev.note11.dart_model_gen_kit.dartmodelgeneratekit.types.ModelArgument
 object DartLangParseUtil {
     private const val DART_CLASS_OR_VARIABLE_REGEX = "^[_a-zA-Z$][_a-zA-Z$0-9]*$"
     private const val DART_CLASS_FIND_REGEX = "class\\s+([A-Z][a-zA-Z0-9_]*)"
+    private const val SINGLE_COMMENT_REGEX = "//.*"
     private const val SINGLE_LINE_COMMENT_REGEX = "//.*\\n"
     private const val MULTI_LINE_COMMENT_REGEX = "/\\\\*.*?\\\\*/\\n?"
     private fun getModelFindRegex(className: String) =
@@ -15,16 +16,38 @@ object DartLangParseUtil {
 
     fun findModelArgs(dartCode: String, className: String): List<ModelArgument> {
         val constructorPart = getModelFindRegex(className).findPatternOnce(dartCode) ?: return emptyList()
-        return constructorPart.split(",\n").mapNotNull {
-            ModelArgument.parseRawArgument(it)
+        val rawArguments = cuttingArguments(constructorPart)
+        return rawArguments.mapNotNull(ModelArgument::parseRawArgument)
+    }
+
+    private fun cuttingArguments(dartCode: String): List<String> {
+        var insideGenericBrackets = false
+        var start = 0
+        val parts = mutableListOf<String>()
+
+        for (i in dartCode.indices) {
+            if (dartCode[i] == '<') {
+                insideGenericBrackets = true
+            } else if (dartCode[i] == '>') {
+                insideGenericBrackets = false
+            } else if (!insideGenericBrackets && dartCode[i] == ',') {
+                parts.add(dartCode.substring(start, i).trim())
+                start = i + 1
+            }
         }
+
+        parts.add(dartCode.substring(start).trim())
+        return parts
     }
 
     fun removeCodeComments(dartCode: String): String {
+        val singleCommentPattern = SINGLE_COMMENT_REGEX.toRegex()
         val singleLineCommentPattern = SINGLE_LINE_COMMENT_REGEX.toRegex()
         val multiLineCommentPattern = MULTI_LINE_COMMENT_REGEX.toRegex(RegexOption.DOT_MATCHES_ALL)
-        val singleLineCleared = singleLineCommentPattern.replace(dartCode, "")
-        return multiLineCommentPattern.replace(singleLineCleared, "")
+
+        return singleCommentPattern.replace(dartCode, "")
+            .let { singleLineCommentPattern.replace(it, "") }
+            .let { multiLineCommentPattern.replace(it, "") }
     }
 
     // List<String> -> [String], Map<String, int> -> [String, int]
