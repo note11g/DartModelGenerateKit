a58ac92bf286e671c21c96357e493a74cc19d2d5
김노트 <note11@kakao.com>
2024-06-13 14:54:54 +0900

    Add(package): `@SnakeCaseKey()`, `@snakeKey`

--- dart_package/model_gen_kit/lib/model_gen_kit.dart
+++ dart_package/model_gen_kit/lib/model_gen_kit.dart
@@ -15,6 +15,31 @@ class CustomKey {
   const CustomKey(String key);
 }
 
+/// compatibility with snake_case keys on json
+///
+/// now, support `lowerCamelCase` -> `snake_case` only.
+///
+/// e.g.
+///
+/// ```
+/// @SnakeCaseKey()
+/// required int userId
+///   gen --> json.user_id
+/// ```
+///
+/// or using shortVer: `@snakeKey` instead.
+///
+/// added on `model_gen_kit 0.1.2`
+class SnakeCaseKey {
+  const SnakeCaseKey();
+}
+
+/// compatibility with snake_case keys on json
+///
+/// short version of `@SnakeCaseKey()`
+///
+/// added on `model_gen_kit 0.1.2`
+const SnakeCaseKey snakeKey = SnakeCaseKey();
 class DefaultVal {
   const DefaultVal(dynamic value);
 }
